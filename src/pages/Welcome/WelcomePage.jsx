f9ba102a92216e4a7a4edaa0e00404d608b4fa92
2331107019-dev <ehm2331107019@upatlacomulco.edu.mx>
2026-03-14 09:09:58 -0600

    Actualización del proyecto smarthome-iot1

--- src/pages/Welcome/WelcomePage.jsx
+++ src/pages/Welcome/WelcomePage.jsx
@@ -1,29 +1,40 @@
-import { Typography, Box, Paper } from "@mui/material";
+import { Typography, Box, Paper, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function WelcomePage() {
   return (
-    <Box>
+    <Box sx={{ textAlign: "center", mt: 4 }}>
 
       <Typography variant="h4" gutterBottom>
         Bienvenido al IoT Dashboard
       </Typography>
 
-      <Paper sx={{p:3, mt:2}}>
-        <Typography variant="body1" paragraph>
+      <Paper sx={{ p: 3, mt: 2, maxWidth: 800, mx: "auto" }}>
+        <Typography paragraph>
           Este sistema SmartHome permite monitorear y controlar dispositivos
           inteligentes conectados mediante tecnología IoT (Internet of Things).
         </Typography>
 
-        <Typography variant="body1" paragraph>
+        <Typography paragraph>
           Desde este dashboard puedes visualizar información en tiempo real
           proveniente de sensores como temperatura, humedad y detección de gas.
         </Typography>
 
-        <Typography variant="body1">
+        <Typography paragraph>
           También podrás administrar dispositivos, revisar estadísticas y
-          configurar los parámetros de tu sistema SmartHome de forma sencilla
-          desde una sola plataforma.
+          configurar los parámetros de tu sistema SmartHome de forma sencilla.
         </Typography>
+
+        {/* BOTÓN INICIAR */}
+        <Button
+          variant="contained"
+          size="large"
+          component={Link}
+          to="/dashboard"
+          sx={{ mt: 2 }}
+        >
+          Iniciar
+        </Button>
 
       </Paper>
 
